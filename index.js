be4255658d75020954fd92e69aea100e6740526a
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-07-04 18:21:11 -0400

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
+  return tutorials.map(  
+    tutorial => tutorial.split(' ').map(function (word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+    }
+  ).join(' ')
+  )
   return tutorials
 }
